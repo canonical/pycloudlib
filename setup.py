fdee0ff295beaaa270b91bc241d481735edba5d0
Joshua Powers <josh.powers@canonical.com>
2018-12-28 14:04:58 -0800

    setup.py & requirements: hard code and split out
    This makes the requirements hard coded and pulls simplestreams directly
    from the git repo instead of the unmaintained PyPi repo.

--- setup.py
+++ setup.py
@@ -3,15 +3,37 @@
 import os
 from setuptools import find_packages, setup
 
-PWD = os.path.abspath(os.path.dirname(__name__))
-REQUIREMENTS_FILE = os.path.join(PWD, 'requirements.txt')
-REQUIREMENTS = []
-with open(REQUIREMENTS_FILE, 'r') as req_file:
-    REQUIREMENTS = req_file.read().splitlines()
 
-README_FILE = os.path.join(PWD, 'README.md')
-with open(README_FILE, 'r') as readme:
-    README_TEXT = readme.read()
+def read_readme():
+    """Read and return text of README.md."""
+    pwd = os.path.abspath(os.path.dirname(__name__))
+    readme_file = os.path.join(pwd, 'README.md')
+    with open(readme_file, 'r') as readme:
+        readme_txt = readme.read()
+
+    return readme_txt
+
+
+def gather_deps():
+    """Read requirements.txt and pre-process for setup.
+
+    Returns:
+         list of packages and dependency links.
+
+    """
+    default = open('requirements.txt', 'r').readlines()
+    new_pkgs = []
+    links = []
+    for resource in default:
+        if 'git+https' in resource.strip():
+            links.append(resource)
+        else:
+            new_pkgs.append(resource)
+
+    return new_pkgs, links
+
+
+PKGS, LINKS = gather_deps()
 
 setup(
     name='pycloudlib',
@@ -19,7 +41,7 @@ setup(
     description=(
         'Python library to launch, interact, and snapshot cloud instances'
     ),
-    long_description=README_TEXT,
+    long_description=read_readme(),
     long_description_content_type='text/markdown',
     author='pycloudlib-devs',
     author_email='pycloudlib-devs@lists.launchpad.net',
@@ -27,7 +49,8 @@ setup(
     license='GNU General Public License v3 (GPLv3)',
     packages=find_packages(),
     python_requires='>=3.4',
-    install_requires=REQUIREMENTS,
+    install_requires=PKGS,
+    dependency_links=LINKS,
     zip_safe=True,
     classifiers=[
         'Development Status :: 4 - Beta',
