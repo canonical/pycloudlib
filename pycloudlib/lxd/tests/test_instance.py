611475e88b2b626c791f2cae9b0bb0010c4774c6
Paride Legovini <paride.legovini@canonical.com>
2021-03-25 18:27:49 +0100

    Drop underscore from _m_subp as we're now using it

--- pycloudlib/lxd/tests/test_instance.py
+++ pycloudlib/lxd/tests/test_instance.py
@@ -31,7 +31,7 @@ class TestVirtualMachineXenialAgentOperations:  # pylint: disable=W0212
     @mock.patch("pycloudlib.lxd.instance.subp")
     def test_exec_with_run_command_on_xenial_machine(
         self,
-        _m_subp,
+        m_subp,
         caplog
     ):
         """Test exec does not work with xenial vm."""
@@ -40,12 +40,12 @@ class TestVirtualMachineXenialAgentOperations:  # pylint: disable=W0212
 
         instance._run_command(["test"], None)
         assert self._missing_agent_msg in caplog.text
-        assert _m_subp.call_count == 1
+        assert m_subp.call_count == 1
 
     @mock.patch("pycloudlib.lxd.instance.subp")
     def test_file_pull_with_agent_on_xenial_machine(
         self,
-        _m_subp,
+        m_subp,
         caplog
     ):
         """Test file pull does not work with xenial vm."""
@@ -54,12 +54,12 @@ class TestVirtualMachineXenialAgentOperations:  # pylint: disable=W0212
 
         instance.pull_file("/some/file", "/some/local/file")
         assert self._missing_agent_msg in caplog.text
-        assert _m_subp.call_count == 1
+        assert m_subp.call_count == 1
 
     @mock.patch("pycloudlib.lxd.instance.subp")
     def test_file_push_with_agent_on_xenial_machine(
         self,
-        _m_subp,
+        m_subp,
         caplog
     ):
         """Test file push does not work with xenial vm."""
@@ -68,4 +68,4 @@ class TestVirtualMachineXenialAgentOperations:  # pylint: disable=W0212
 
         instance.push_file("/some/file", "/some/local/file")
         assert self._missing_agent_msg in caplog.text
-        assert _m_subp.call_count == 1
+        assert m_subp.call_count == 1
