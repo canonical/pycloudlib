1493221528892148006be299fc34f794e362f799
Alec Warren <alec.warren@canonical.com>
2024-04-04 11:36:42 -0400

    whoopsie. thanks brett!

--- .github/workflows/version_check.py
+++ .github/workflows/version_check.py
@@ -9,7 +9,7 @@ def compare_versions(old_version, new_version):
     minor_release = new_version.minor > old_version.minor
     micro_release = new_version.micro > old_version.micro
     if sum([major_release, minor_release, micro_release]) > 1:
-        return "Only one digit can be incremented at a time.
+        return "Only one digit can be incremented at a time."
     if major_release:
          if new_version.minor != 0 or new_version.micro != 0:
              return f"Major version increased. Expected minor and micro versions to be 0. Found: {new_version}"
