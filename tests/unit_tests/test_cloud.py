canonical/pycloudlib

commit 335c4a4d9525d78120cc127905ccbd1928046e2d

feat: add ability to check for multiple default ssh key locations
Since Ubuntu 24.04 LTS and newer, the default location for ssh keys is
`~/.ssh/id_ed25519` instead of `~/.ssh/id_rsa`. Thus, when no ssh key
path is specified, this commit changes pycloudlib to check for an
existing ssh key at either  `~/.ssh/id_ed25519` or `~/.ssh/id_rsa`.

Fixes GH #341

VERSION
@@ -1,1 +1,1 @@
-1!10.0.5
+1!10.1.0

pycloudlib/cloud.py
@@ -13,7 +13,11 @@ from typing import Any, List, Optional, Sequence
 import paramiko
 
 from pycloudlib.config import ConfigFile, parse_config
-from pycloudlib.errors import CleanupError, InvalidTagNameError
+from pycloudlib.errors import (
+    CleanupError,
+    InvalidTagNameError,
+    PycloudlibError,
+)
 from pycloudlib.instance import BaseInstance
 from pycloudlib.key import KeyPair
 from pycloudlib.util import (
@@ -59,17 +63,10 @@ class BaseCloud(ABC):
         self._log = logging.getLogger("{}.{}".format(__name__, self.__class__.__name__))
         self._check_and_set_config(config_file, required_values)
 
-        user = getpass.getuser()
-        self.key_pair = KeyPair(
-            public_key_path=os.path.expandvars(
-                self.config.get("public_key_path", f"~{user}/.ssh/id_rsa.pub")
-            ),
-            private_key_path=os.path.expandvars(self.config.get("private_key_path", "")),
-            name=self.config.get("key_name", user),
-        )
-
         self.tag = get_timestamped_tag(tag) if timestamp_suffix else tag
         self._validate_tag(self.tag)
+
+        self.key_pair = self._get_ssh_keys()
 
     def __enter__(self):
         """Enter context manager for this class."""
@@ -312,3 +309,34 @@ class BaseCloud(ABC):
 
         if rules_failed:
             raise InvalidTagNameError(tag=tag, rules_failed=rules_failed)
+
+    def _get_ssh_keys(self) -> KeyPair:
+        user = getpass.getuser()
+        # check if id_rsa or id_ed25519 keys exist in the user's .ssh directory
+        possible_default_keys = [
+            os.path.expanduser("~/.ssh/id_rsa.pub"),
+            os.path.expanduser("~/.ssh/id_ed25519.pub"),
+        ]
+        public_key_path: Optional[str] = os.path.expanduser(self.config.get("public_key_path", ""))
+        if not public_key_path:
+            for pubkey in possible_default_keys:
+                if os.path.exists(pubkey):
+                    self._log.info("No public key path provided, using: %s", pubkey)
+                    public_key_path = pubkey
+                    break
+            if not public_key_path:
+                raise PycloudlibError(
+                    "No public key path provided and no key found in default locations: "
+                    "'~/.ssh/id_rsa.pub' or '~/.ssh/id_ed25519.pub'"
+                )
+        if not os.path.exists(os.path.expanduser(public_key_path)):
+            raise PycloudlibError(f"Provided public key path '{public_key_path}' does not exist")
+        if public_key_path not in possible_default_keys:
+            self._log.info("Using provided public key path: '%s'", public_key_path)
+        private_key_path = self.config.get("private_key_path", "")
+
+        return KeyPair(
+            public_key_path=public_key_path,
+            private_key_path=private_key_path,
+            name=self.config.get("key_name", user),
+        )

tests/unit_tests/test_cloud.py
@@ -46,6 +46,7 @@ class CloudSubclass(BaseCloud):
         """Skeletal list_keys."""
 
 
+@pytest.mark.mock_ssh_keys
 class TestBaseCloud:
     """Tests covering BaseCloud intialization."""
 
@@ -82,8 +83,16 @@ class TestBaseCloud:
         mycloud = CloudSubclass(config_file=StringIO(CONFIG), **args)
         assert expected_tag == mycloud.tag
 
+    @pytest.mark.dont_mock_ssh_keys
     @mock.patch(MPATH + "getpass.getuser", return_value="root")
-    def test_init_sets_key_pair_based_on_getuser(self, _m_getuser):
+    @mock.patch("os.path.expanduser", side_effect=lambda x: x.replace("~", "/root"))
+    @mock.patch("os.path.exists", side_effect=lambda x: "/.ssh/id_rsa" in x)
+    def test_init_sets_key_pair_based_on_getuser(
+        self,
+        _m_getuser,
+        _m_expanduser,
+        _m_exists,
+    ):
         """
         The default key_pair for the cloud is based on the current user.
 
@@ -96,7 +105,39 @@ class TestBaseCloud:
         assert mycloud.key_pair.private_key_path == ("/root/.ssh/id_rsa")
         assert mycloud.key_pair.public_key_path == ("/root/.ssh/id_rsa.pub")
 
-    def test_init_sets_key_pair_from_config(self):
+    @pytest.mark.dont_mock_ssh_keys
+    @mock.patch("os.path.expanduser", side_effect=lambda x: x.replace("~", "/root"))
+    @mock.patch("os.path.exists", side_effect=lambda x: "/.ssh/id_ed25519" in x)
+    @mock.patch(MPATH + "getpass.getuser", return_value="root")
+    def test_init_can_use_id_ed25519_key(
+        self,
+        _m_getuser,
+        _m_expanduser,
+        _m_exists,
+    ):
+        """
+        Validates that key_pair uses the id_ed25519 key if id_rsa doesn't exist
+
+        To do this, we mock the os.path.exists function to return True ONLY
+        for the id_ed25519 key to simulate the absence of the id_rsa key.
+
+        """
+        mycloud = CloudSubclass(
+            tag="tag",
+            timestamp_suffix=False,
+            config_file=StringIO(CONFIG),
+        )
+
+        id_ed25519_path = "/root/.ssh/id_ed25519.pub"
+
+        assert mycloud.key_pair.name == "root"
+        assert mycloud.key_pair.public_key_path == id_ed25519_path
+        assert mycloud.key_pair.private_key_path == id_ed25519_path.replace(".pub", "")
+
+    @pytest.mark.dont_mock_ssh_keys
+    @mock.patch("os.path.expanduser", side_effect=lambda x: x.replace("~", "/root"))
+    @mock.patch("os.path.exists", side_effect=lambda x: "/.ssh/id_rsa" in x)
+    def test_init_sets_key_pair_from_config(self, _m_expanduser, _m_exists):
         """The key_pair is set from the config file."""
         mycloud = CloudSubclass(
             tag="tag",
@@ -117,7 +158,10 @@ class TestBaseCloud:
         assert mycloud.key_pair.public_key_path == "/home/asdf/.ssh/id_rsa.pub"
         assert mycloud.key_pair.private_key_path == "/home/asdf/.ssh/my_key"
 
-    def test_missing_private_key_in_ssh_config(self):
+    @pytest.mark.dont_mock_ssh_keys
+    @mock.patch("os.path.expanduser", side_effect=lambda x: x.replace("~", "/root"))
+    @mock.patch("os.path.exists", side_effect=lambda x: "/.ssh/id_rsa" in x)
+    def test_missing_private_key_in_ssh_config(self, _m_expanduser, _m_exists):
         """The key_pair assumes the private key name."""
         mycloud = CloudSubclass(
             tag="tag",

tests/unit_tests/azure/test_cloud.py
@@ -26,6 +26,7 @@ compute_client_mock = mock.MagicMock()
 
 # Disable this one because we're intentionally testing a protected member
 # pylint: disable=protected-access
+@pytest.mark.mock_ssh_keys
 class TestCreateNetworkInterfaceClient:
     """Tests covering _create_network_interface_client method."""
 
@@ -126,6 +127,7 @@ class TestCreateNetworkInterfaceClient:
         assert expected_calls == network_group_mock.begin_create_or_update.call_args_list
 
 
+@pytest.mark.mock_ssh_keys
 @mock.patch(
     "pycloudlib.azure.util.get_client",
     side_effect=[

tests/unit_tests/gce/test_cloud.py
@@ -51,6 +51,7 @@ def gce(request, common_mocks, tmpdir):
 
 
 # pylint: disable=protected-access,missing-function-docstring
+@pytest.mark.mock_ssh_keys
 class TestGCE:
     """General GCE testing."""
 

tests/unit_tests/lxd/test_cloud.py
@@ -23,6 +23,7 @@ def does_not_raise():
     yield
 
 
+@pytest.mark.mock_ssh_keys
 class TestLaunch:
     """Tests covering pycloudlib.lxd.cloud.launch method."""
 
@@ -80,6 +81,7 @@ class TestLaunch:
             assert inst.start.call_count == 0
 
 
+@pytest.mark.mock_ssh_keys
 class TestProfileCreation:
     """Tests covering pycloudlib.lxd.cloud.create_profile method."""
 
@@ -139,6 +141,7 @@ class TestProfileCreation:
         ]
 
 
+@pytest.mark.mock_ssh_keys
 class TestReleaseImage:
     @pytest.mark.parametrize(
         "cloud_cls,release,arch,image_type,expected_kwargs",
@@ -198,6 +201,7 @@ class TestReleaseImage:
         find_last_fingerprint.assert_called_once_with(**expected_kwargs)
 
 
+@pytest.mark.mock_ssh_keys
 class TestDailyImage:
     @pytest.mark.parametrize(
         "cloud_cls,release,arch,image_type,expected_kwargs",

tests/unit_tests/openstack/test_cloud.py
@@ -16,6 +16,7 @@ network = ""
 """
 
 
+@pytest.mark.mock_ssh_keys
 @mock.patch(
     "pycloudlib.key.KeyPair.public_key_content",
     new_callable=mock.PropertyMock,

tests/conftest.py
@@ -1,3 +1,23 @@
 import logging
+import pytest
+
+from pycloudlib.key import KeyPair
 
 logging.basicConfig(level=logging.NOTSET)
+
+
+@pytest.fixture(name="fake_ssh_keys", autouse=True)
+def fake_ssh_keys_fixture(mocker, request):
+    """Fixture to mock `_get_ssh_keys` across all tests unless marked otherwise."""
+    if (
+        "mock_ssh_keys" in request.node.keywords
+        and "dont_mock_ssh_keys" not in request.node.keywords
+    ):
+        mocker.patch(
+            "pycloudlib.cloud.BaseCloud._get_ssh_keys",
+            return_value=KeyPair(
+                name="fake_key",
+                public_key_path="/fake/public/key",
+                private_key_path="/fake/private/key",
+            ),
+        )

commit 5e10d1d76977529bec23ea5fbbeb92b70e6189f4

fix(ssh): don't raise exception when default ssh keys can't be found
This commit will once again allow for setting ssh keys at runtime
using the `Cloud.use_key()` method when default ssh keys dont exist
on the system and no ssh key paths are provided in pycloudlib.toml.

Further context:
PR #406 broke some end consumers/users of pycloudlib on systems where
1) ssh keys are not set in the pycloudlib.toml and the ssh key is later
set using the Cloud.use_key() method
2) no ssh keys exist at the default paths of '~/.ssh/id_rsa.pub' or
'~/.ssh/id_ed25519.pub'.
This commit removes / reverts the error that was introduced in PR #406
which gets raised at the Cloud class instantation when those two cases
are true. The biggest issue with this change, is there was no way for
an end user to override/prevent pycloudlib from erroring out if those
cases are true - besides for creating a fake ssh key at one of the two
default paths. Now, a warning is just logged instead, restoring the
flexibility pycloudlib preivously provided to end users for setting /
using ssh keys.

If both scenarios 1 and 2 are true (ssh key is unset/doesn't exist),
a new exception type `UnsetSSHKeyError` will be raised with verbose
exception message explaining why this happened and how to fix.

VERSION
@@ -1,1 +1,1 @@
-1!10.5.0
+1!10.6.0

pycloudlib/cloud.py
@@ -66,7 +66,11 @@ class BaseCloud(ABC):
         self.tag = get_timestamped_tag(tag) if timestamp_suffix else tag
         self._validate_tag(self.tag)
 
-        self.key_pair = self._get_ssh_keys()
+        self.key_pair = self._get_ssh_keys(
+            public_key_path=self.config.get("public_key_path", ""),
+            private_key_path=self.config.get("private_key_path", ""),
+            name=self.config.get("key_name", getpass.getuser()),
+        )
 
     def __enter__(self):
         """Enter context manager for this class."""
@@ -251,7 +255,11 @@ class BaseCloud(ABC):
             name: name to reference key by
         """
         self._log.debug("using SSH key from %s", public_key_path)
-        self.key_pair = KeyPair(public_key_path, private_key_path, name)
+        self.key_pair = self._get_ssh_keys(
+            public_key_path=public_key_path,
+            private_key_path=private_key_path,
+            name=name,
+        )
 
     def _check_and_set_config(
         self,
@@ -310,14 +318,37 @@ class BaseCloud(ABC):
         if rules_failed:
             raise InvalidTagNameError(tag=tag, rules_failed=rules_failed)
 
-    def _get_ssh_keys(self) -> KeyPair:
-        user = getpass.getuser()
-        # check if id_rsa or id_ed25519 keys exist in the user's .ssh directory
+    def _get_ssh_keys(
+        self,
+        public_key_path: Optional[str] = None,
+        private_key_path: Optional[str] = None,
+        name: Optional[str] = None,
+    ) -> KeyPair:
+        """Retrieve SSH key pair paths.
+
+        This method attempts to retrieve the paths to the public and private SSH keys.
+        If no public key path is provided, it will look for default keys in the user's
+        `~/.ssh` directory. If no keys are found, it logs a warning and returns a KeyPair
+        with None values.
+
+        Args:
+            public_key_path (Optional[str]): The path to the public SSH key. If not provided,
+                the method will search for default keys.
+            private_key_path (Optional[str]): The path to the private SSH key. Defaults to None.
+            name (Optional[str]): An optional name for the key pair. Defaults to None.
+
+        Returns:
+            KeyPair: An instance of KeyPair containing the paths to the public and private keys,
+            and the optional name.
+
+        Raises:
+            PycloudlibError: If the provided public key path does not exist.
+        """
         possible_default_keys = [
             os.path.expanduser("~/.ssh/id_rsa.pub"),
             os.path.expanduser("~/.ssh/id_ed25519.pub"),
         ]
-        public_key_path: Optional[str] = os.path.expanduser(self.config.get("public_key_path", ""))
+        public_key_path = os.path.expanduser(public_key_path or "")
         if not public_key_path:
             for pubkey in possible_default_keys:
                 if os.path.exists(pubkey):
@@ -325,18 +356,19 @@ class BaseCloud(ABC):
                     public_key_path = pubkey
                     break
             if not public_key_path:
-                raise PycloudlibError(
+                self._log.warning(
                     "No public key path provided and no key found in default locations: "
-                    "'~/.ssh/id_rsa.pub' or '~/.ssh/id_ed25519.pub'"
+                    "'~/.ssh/id_rsa.pub' or '~/.ssh/id_ed25519.pub'. SSH key authentication will "
+                    "not be possible unless a key is later provided with the 'use_key' method."
                 )
+                return KeyPair(None, None, None)
         if not os.path.exists(os.path.expanduser(public_key_path)):
             raise PycloudlibError(f"Provided public key path '{public_key_path}' does not exist")
         if public_key_path not in possible_default_keys:
             self._log.info("Using provided public key path: '%s'", public_key_path)
-        private_key_path = self.config.get("private_key_path", "")
 
         return KeyPair(
             public_key_path=public_key_path,
             private_key_path=private_key_path,
-            name=self.config.get("key_name", user),
+            name=name,
         )

pycloudlib/errors.py
@@ -156,3 +156,18 @@ class InvalidTagNameError(PycloudlibException):
     def __str__(self) -> str:
         """Return string representation of the error."""
         return f"Tag '{self.tag}' failed the following rules: {', '.join(self.rules_failed)}"
+
+
+class UnsetSSHKeyError(PycloudlibException):
+    """Raised when a SSH key is unset and no default key can be found."""
+
+    def __str__(self) -> str:
+        """Return string representation of the error."""
+        return (
+            "No public key content available for unset key pair. This error occurs when no SSH "
+            "key is provided in the pycloudlib.toml file and no default keys can be found on "
+            "the system. If you wish to provide custom SSH keys at runtime, you can do so by "
+            "calling the `use_key` method on the `Cloud` class. If you wish to use default SSH "
+            "keys, make sure they are present on the system and that they are located in the "
+            "default locations."
+        )

pycloudlib/key.py
@@ -2,12 +2,20 @@
 """Base Key Class."""
 
 import os
+from typing import Optional
+
+from pycloudlib.errors import UnsetSSHKeyError
 
 
 class KeyPair:
     """Key Class."""
 
-    def __init__(self, public_key_path, private_key_path=None, name=None):
+    def __init__(
+        self,
+        public_key_path: Optional[str],
+        private_key_path: Optional[str] = None,
+        name: Optional[str] = None,
+    ):
         """Initialize key class to generate key or reuse existing key.
 
         The public key path is given then the key is stored and the
@@ -21,11 +29,15 @@ class KeyPair:
         """
         self.name = name
         self.public_key_path = public_key_path
-        if private_key_path:
-            self.private_key_path = private_key_path
-        else:
-            self.private_key_path = self.public_key_path.replace(".pub", "")
 
+        # don't set private key path if public key path is None (ssh key is unset)
+        if self.public_key_path is None:
+            self.private_key_path = None
+            return
+
+        self.private_key_path = private_key_path or self.public_key_path.replace(".pub", "")
+
+        # Expand user paths after setting private key path
         self.public_key_path = os.path.expanduser(self.public_key_path)
         self.private_key_path = os.path.expanduser(self.private_key_path)
 
@@ -40,7 +52,8 @@ class KeyPair:
         """Read the contents of the public key.
 
         Returns:
-            output of public key
-
+            str: The public key content
         """
+        if self.public_key_path is None:
+            raise UnsetSSHKeyError()
         return open(self.public_key_path, encoding="utf-8").read()

tests/unit_tests/test_cloud.py
@@ -1,14 +1,15 @@
 """Tests related to pycloudlib.cloud module."""
 
 from io import StringIO
+import logging
 from textwrap import dedent
-from typing import List
+from typing import List, Optional
 
 import mock
 import pytest
 
 from pycloudlib.cloud import BaseCloud
-from pycloudlib.errors import InvalidTagNameError
+from pycloudlib.errors import InvalidTagNameError, UnsetSSHKeyError
 
 # mock module path
 MPATH = "pycloudlib.cloud."
@@ -180,6 +181,30 @@ class TestBaseCloud:
         assert mycloud.key_pair.name == "some_name"
         assert mycloud.key_pair.public_key_path == "/home/asdf/.ssh/id_rsa.pub"
         assert mycloud.key_pair.private_key_path == "/home/asdf/.ssh/id_rsa"
+
+    @pytest.mark.dont_mock_ssh_keys
+    @mock.patch("os.path.expanduser", side_effect=lambda x: x.replace("~", "/root"))
+    @mock.patch("os.path.exists", return_value=False)
+    def test_init_raises_error_when_no_ssh_keys_found(
+        self,
+        _m_expanduser,
+        _m_exists,
+        caplog,
+    ):
+        """
+        Test that an error is raised when no SSH keys can be found.
+
+        This test verifies that an error is raised when no SSH keys can be found in the default
+        locations and no public key path is provided in the config file.
+        """
+        # set log level to Warning to ensure warning gets logged
+        caplog.set_level(logging.WARNING)
+        with pytest.raises(UnsetSSHKeyError) as exc_info:
+            cloud = CloudSubclass(tag="tag", timestamp_suffix=False, config_file=StringIO(CONFIG))
+            # now we try to access the public key content to trigger the exception
+            cloud.key_pair.public_key_content
+        assert "No public key path provided and no key found in default locations" in caplog.text
+        assert "No public key content available for unset key pair." in str(exc_info.value)
 
     rule1 = "All letters must be lowercase"
     rule2 = "Must be between 1 and 63 characters long"