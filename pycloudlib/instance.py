325175d31076937bd91f304c2e32c3463b4a8943
a-dubs <alecwarren19@gmail.com>
2025-02-03 12:24:26 -0500

    feat(instance): add stub of new optional instance property "private_ip"

--- pycloudlib/instance.py
+++ pycloudlib/instance.py
@@ -58,6 +58,11 @@ class BaseInstance(ABC):
     def id(self) -> str:
         """Return instance id."""
         raise NotImplementedError
+    
+    @property
+    def private_ip(self) -> str:
+        """Return instance private ip."""
+        raise NotImplementedError
 
     @property
     @abstractmethod
